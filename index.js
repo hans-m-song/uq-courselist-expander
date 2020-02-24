a9ab0eee7cc7737ef0ab6d490a66183b6325567c
Hans Song <hans.m.song@gmail.com>
2020-02-24 12:45:19 +1000

    async'd loading data and appended to table

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 (() => {
     const selectors = {
-        courselist: '.courselist',
-        course: 'tbody > tr:not(.option.or)',
+        header: 'thead tr',
+        courselist: '.courselist table',
+        course: 'tbody > tr',
         summary: '#summary [id^=course]',
         description: '#description .course-offering-year',
     };
+    const parser = new DOMParser();
 
-    const scrapeCourseData = (document) => {
-        const summary = Array.from(document.querySelectorAll(selectors.summary))
+    const scrapeCourseData = (dom) => {
+        const summary = Array.from(dom.querySelectorAll(selectors.summary))
             .reduce((summary, el) => {
                 summary[el.id.replace(/course-/, '')] = el.innerText;
                 return summary;
             }, {});
         
-        const archivedOfferings = Array.from(document.querySelectorAll(selectors.description))
+        const archivedOfferings = Array.from(dom.querySelectorAll(selectors.description))
             .map((offering) => offering.innerText.trim());
         
         summary.semesters = Array.from(archivedOfferings.reduce((set, offering) => 
@@ -22,21 +24,60 @@
         return {summary, offerings: archivedOfferings};
     };
 
-    const applyToRow = async (parser, row) => {
-        const link = row.querySelector('a');
-        const response = await fetch(link);
+    const appendCell = (row, callback) => {
+        const td = document.createElement('td');
+        row.appendChild(td);
+        callback.then((data) => td.innerText = data);
+    }
+
+    const fetchData = async (code, url) => {
+        const response = await fetch(url);
         const text = await response.text();
         const dom = parser.parseFromString(text, 'text/html');
-        const data = scrapeCourseData(dom);
+        const data = {
+            code,
+            ...scrapeCourseData(dom),
+        };
         console.log(data);
+        return data;
+    }
+
+    const appendData = (row, dataPromise) => {
+        appendCell(row, dataPromise.then((data) => data.summary.prerequisite || ''));
+        appendCell(row, dataPromise.then((data) => data.summary.semesters.join(', ').replace(/\s|Semester/g, '')));
+    };
+
+    const applyToRow = async (row) => {
+        if (row.className.includes('or')) {
+            row.querySelector('td').colSpan += 2;
+            return; 
+        }
+
+        const link = row.querySelector('a');
+        const url = link.href
+        const code = link.innerText
+        if (!url) {
+            console.log('no data found for course', code);
+            return;
+        }
+        
+        appendData(row, fetchData(code, url));
+    };
+
+    const appendHeaders = (table) => {
+        const header = table.querySelector(selectors.header);
+        appendCell(header, Promise.resolve('Prerequisite'));
+        appendCell(header, Promise.resolve('Semesters'));
+
+        return table;
     };
 
     const scrapePage = () => {
-        const parser = new DOMParser();
         Array.from(document.querySelectorAll(selectors.courselist))
             .forEach((courselist) =>
-                Array.from(courselist.querySelectorAll(selectors.course))
-                    .forEach((course) => applyToRow(parser, course)));
+                Array.from(appendHeaders(courselist)
+                    .querySelectorAll(selectors.course))
+                    .forEach((course) => applyToRow(course)));
 
     };
 
